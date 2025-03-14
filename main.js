d1179652652b35d890f92417197b32bdd485898c
mikhail0404 <mikhail0404@yandex.ru>
2025-03-14 16:55:39 +0300

    Changes to be committed: 	modified:   main.js 	modified:   package-lock.json 	modified:   package.json

--- main.js
+++ main.js
@@ -2,9 +2,7 @@ require("dotenv").config();
 //twitch
 const tmi = require("tmi.js");
 const fs = require("fs");
-const express = require("express");
 //
-const app = express();
 const io = require("socket.io-client");
 const socket = io.connect(
   "wss://socket.donationalerts.ru:443",
@@ -17,71 +15,62 @@ const socket = io.connect(
   }
 );
 
-const port = 10000;
-const host = "localhost";
 
+const options = {
+  options: {
+    debug: true,
+  },
+  connection: {
+    cluster: "aws",
+    reconnect: true,
+  },
+  identity: {
+    username: "", //имя бота
+    password: "", //токен бота
+  },
+  channels: [""], //каналы которые парсит бот
+};
 
+options.identity.username = process.env.username;
+options.identity.password = process.env.tokenBot;
+options.channels[0] = process.env.channels;
 
-app.listen(port, host, () => {
-  console.log(`Server running on http://${host}:${port}`);
-  const options = {
-    options: {
-      debug: true,
-    },
-    connection: {
-      cluster: "aws",
-      reconnect: true,
-    },
-    identity: {
-      username: "", //имя бота
-      password: "", //токен бота
-    },
-    channels: [""], //каналы которые парсит бот
-  };
-  
-  options.identity.username = process.env.username;
-  options.identity.password = process.env.tokenBot;
-  options.channels[0] = process.env.channels;
-  
-  socket.emit("add-user", {
-    token: process.env.tokenDonate, //donation alerts токен
-    type: "minor",
-  });
-  
-  const client = new tmi.client(options);
-  client.connect();
-  console.log("Connected to Twitch succefully end");
-  
-  //donation alerts
-  //Connect
-  socket.on("connect", function (data) {
-    console.log("Connected to Donation Alerts");
-  });
-  //Error
-  socket.on("connect_error", (err) => {
-    console.error(`Donation Alerts Connection Error! ${err.message}`);
-    process.exit(0);
-  });
-  //Disconnect
-  socket.on("disconnect", () => {
-    console.log("Donation Alerts Disconnected!");
-    process.exit(0);
-  });
-  
-  socket.on("donation", function (msg) {
-    donate = JSON.parse(msg);
-    // console.log(donate);
-    if (client) {
-      client.action(
-        options.channels[0],
-        (donate.username ? donate.username : "Анон") +
-          " - " +
-          "[" +
-          donate.amount +
-          (donate.currency === "RUB" ? "" : " " + donate.currency) +
-          "]" +
-          (donate.message ? " - " + donate.message : " *молча*")
-      );
-    }
-  });
+socket.emit("add-user", {
+  token: process.env.tokenDonate, //donation alerts токен
+  type: "minor",
+});
+
+const client = new tmi.client(options);
+client.connect();
+console.log("Connected to Twitch succefully end");
+
+//donation alerts
+//Connect
+socket.on("connect", function (data) {
+  console.log("Connected to Donation Alerts");
+});
+//Error
+socket.on("connect_error", (err) => {
+  console.error(`Donation Alerts Connection Error! ${err.message}`);
+  process.exit(0);
+});
+//Disconnect
+socket.on("disconnect", () => {
+  console.log("Donation Alerts Disconnected!");
+  process.exit(0);
+});
+
+socket.on("donation", function (msg) {
+  donate = JSON.parse(msg);
+  // console.log(donate);
+    client.action(
+      options.channels[0],
+      (donate.username ? donate.username : "Анон") +
+        " - " +
+        "[" +
+        donate.amount +
+        (donate.currency === "RUB" ? "" : " " + donate.currency) +
+        "]" +
+        (donate.message ? " - " + donate.message : " *молча*")
+    );
 });
